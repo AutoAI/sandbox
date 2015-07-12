844fd8c70d89f788a538b2b1eb0fcd174f1fead9
Travis Vanderstad <t.vanderstad@live.com>
2015-07-11 22:16:26 -0400

    fixed integer arithmetic error with chi square difference

--- Travis/histogram_cluster.h
+++ Travis/histogram_cluster.h
@@ -8,7 +8,7 @@ class HistogramCluster{
 public:
 	HistogramCluster(int x_resolution, int y_resolution, int block_dimension, int num_blocks_x, int num_blocks_y, int num_bins);
 
-	uint16_t *doCluster(uint8_t *frame_buffer, int closeness_threshold, int blindness_threshold);
+	uint16_t *doCluster(uint8_t *frame_buffer, float closeness_threshold, int blindness_threshold);
 	
 
 private:
