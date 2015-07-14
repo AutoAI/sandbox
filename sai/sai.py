c8af87ccbcde9cfc4b1565aa706a76d06da4e8dd
Sai susheel Kode <saisusheelkode@Sais-MacBook-Pro.local>
2015-07-14 16:04:22 -0500

    I went through like different exercises on python where I learnt on strings, print statements, conditions etc..

--- sai/sai.py
+++ sai/sai.py
@@ -1,4 +1,4 @@
-print "I have to complete this exercise today."
+"""print "I have to complete this exercise today."
 
 # exercise 3 
 
@@ -20,4 +20,86 @@ print " how about some more."
 
 print " is it greater?", 5 > -2 
 print " is it greater or equal?", 5 >= -2 
-print " is it less or equal?", 5 <= -2
+print " is it less or equal?", 5 <= -2 """
+
+"""cars = 100
+space_in_a_car = 4.0 
+drivers = 30 
+passengers = 90 
+cars_not_driven = cars - drivers 
+cars_driven = drivers 
+carpool_capacity = cars_driven * space_in_a_car 
+average_passengers_per_car = passengers / cars_driven 
+
+print "there are", cars,"cars available." """
+
+# traceback error : name car_pool_capacity is not defined : because carpool_capacity is defined  which is lot more difficult with using an other underscore in between car and pool
+
+ # exercise 5 
+"""my_name = 'sai'
+my_age = 24 
+my_height = 174.38 # cms 
+my_weight = 164.23 # pounds
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "lets talk about %s." % my_name 
+print "he is %d inches tall." % my_height
+print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+# Lets say I just need to print this line without commenting the before lines is there any way to just print this line 
+
+print "If I add", my_age, "%d, and %d I get %d." % (my_height, my_age + my_height, my_weight) 
+
+x = "there are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "those who knows %s and those who %s." % (binary, do_not)
+
+
+print x 
+print y 
+
+print "I said: %r." % x
+print "I also said: '%s'." % y 
+
+hilarious = False 
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "this is the left side of ..."
+e = "a string with a right side."
+
+print w + e
+
+
+print "marry had a little lamb."
+print "." * 10 
+
+end1 = "c"
+end2 = "j"
+end3 = "p"
+end4 = "f"
+
+print end1 + end2 
+print end3 + """
+
+formatter = "%r %r %r"
+
+print formatter % (1,2,3)
+print formatter % ("one", "two", "three")
+
+days = "Mon Tue Wed thu fri sat sun"
+
+print "here are the days:", days 
+
+
+
+
+
+
+
+
+
