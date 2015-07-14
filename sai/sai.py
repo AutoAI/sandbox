92ce1407ea39d1bdf16f74438a2cbd1595d30e56
Sai susheel Kode <saisusheelkode@Sais-MacBook-Pro.local>
2015-07-14 11:15:58 -0500

    is there any way that i can only print a single line without using the comments to the previous line?

---
+++ sai/sai.py
@@ -0,0 +1,23 @@
+print "I have to complete this exercise today."
+
+# exercise 3 
+
+print "hens", 25.0 + 30 / 9
+print "rosters", 100 -25 * 3 % 5
+
+print "now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+
+print 3 + 2 < 5 - 7 
+
+print " what is 3 + 2?", 3 + 2
+print " what is 5 - 7?", 5 - 7
+
+print "oh, that's why it is false."
+
+print " how about some more."
+
+print " is it greater?", 5 > -2 
+print " is it greater or equal?", 5 >= -2 
+print " is it less or equal?", 5 <= -2
